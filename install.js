fb1e9b7907a8ec307798983a7286fff1d972f247
Mohseen Mukaddam <mohseenmukaddam6@gmail.com>
2017-05-18 15:48:16 -0700

    logging

--- install.js
+++ install.js
@@ -40,6 +40,10 @@ console.log('\nDEBUG :', process.env.npm_config_platform);
 console.log('\nDEBUG :', process.env.npm_config_arch);
 console.log('\nDEBUG :', process.env.npm_config_strict_ssl);
 
+fs.appendFileSync('./temp.log', process.env.npm_config_platform);
+fs.appendFileSync('./temp.log', process.env.npm_config_arch);
+fs.appendFileSync('./temp.log', process.env.npm_config_strict_ssl);
+
 download({version: version, platform: process.env.npm_config_platform, arch: process.env.npm_config_arch, strictSSL: process.env.npm_config_strict_ssl === 'true'}, extractFile)
 
 // unzips and makes path.txt point at the correct executable
